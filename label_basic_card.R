0139b6dab5383797a87cd0fcd74fd20e389f4c0a
Eduardo-Alanis-Garcia <eag.eleven1@gmail.com>
2025-09-20 13:28:18 -0600

    para subir imagenes

---
+++ label_basic_card.R
@@ -0,0 +1,41 @@
+mun = sf::read_sf("Datos/Municipios/municipiosjair.shp")
+
+archivos = list.files(path = "Img/Municipios/", pattern = "\\.png$", full.names = T, recursive = T) |>  data.frame()
+archivos = archivos |> 
+  dplyr::rename(img_direccion = list.files.path....Img.Municipios....pattern.......png....full.names...T..)
+
+archivos = archivos |> 
+  dplyr::mutate(municipio = basename(img_direccion) |> gsub(pattern = ".png", replacement = "") |>  stringr::str_squish())
+
+
+mun = mun |> 
+  dplyr::left_join(y = archivos, by = c("NOM_MUN" = "municipio")) 
+
+mun = mun |> 
+  dplyr::select(CVEGEO, NOM_MUN, Area, PERIMETER, img_direccion, geometry)
+
+
+
+library(leaflet)
+
+mapa = leaflet() |> 
+  addTiles() |> 
+  addPolygons(data = mun, 
+              label = paste0(
+                "<div style='display:flex; align-items:center;'>",
+                "<img src='", mun$img_b64, "' style='width:30px; height:30px; margin-right:5px;'>",
+                "<span>", "<b>", mun$NOM_MUN, "</b>", "</span>",
+                "</div>"
+              ) |> lapply(FUN =  function(x){htmltools::HTML(x)}
+              ))
+mapa
+mun$img_direccion
+
+library(base64enc)
+mun$img_b64 <- sapply(mun$img_direccion, function(path) {
+  dataURI(file = path, mime = "image/png")
+})
+
+
+
+paste0(, mun$img_direccion[1])
